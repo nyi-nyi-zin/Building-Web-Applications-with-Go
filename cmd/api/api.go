9c7568d0f5e803bfee67c3ad9a67ff4bccac10a5
Nyi Nyi Zin <nyinyizin1818@gmail.com>
2025-06-01 22:34:28 +0630

    reviewing fixing

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -9,54 +9,51 @@ import (
 	"time"
 )
 
+const version = "1.0.0"
 
-	const version = "1.0.0"
-	
-
-	type config struct { 
-		port int
-		env  string
-		db   struct { 
-			dsn string 
-		}
-		stripe struct { 
-			secret string 
-			key     string 
-		}
+type config struct {
+	port int
+	env  string
+	db   struct {
+		dsn string
 	}
-
-	type application struct { 
-		config        config                      
-		infoLog       *log.Logger                  
-		errorLog      *log.Logger                 
-		version       string                        
+	stripe struct {
+		secret string
+		key    string
 	}
+}
 
-	func (app *application) serve() error { 
-	srv := &http.Server{ 
-		Addr:              fmt.Sprintf(":%d", app.config.port), 
-		Handler:           app.routes(),                        
-		IdleTimeout:       30 * time.Second,                  
-		ReadTimeout:       10 * time.Second,                 
-		ReadHeaderTimeout: 5 * time.Second,                     
-		WriteTimeout:      5 * time.Second,                  
+type application struct {
+	config   config
+	infoLog  *log.Logger
+	errorLog *log.Logger
+	version  string
+}
+
+func (app *application) serve() error {
+	srv := &http.Server{
+		Addr:              fmt.Sprintf(":%d", app.config.port),
+		Handler:           app.routes(),
+		IdleTimeout:       30 * time.Second,
+		ReadTimeout:       10 * time.Second,
+		ReadHeaderTimeout: 5 * time.Second,
+		WriteTimeout:      5 * time.Second,
 	}
 	app.infoLog.Println(fmt.Sprintf("Starting Back end server in %s mode on port %d", app.config.env, app.config.port))
 
-	return srv.ListenAndServe() 
+	return srv.ListenAndServe()
 }
 
+func main() {
 
-func main(){
+	var cfg config
 
-	var cfg config 
-
+	//
 	flag.IntVar(&cfg.port, "port", 4001, "Server port to listen on")
 	flag.StringVar(&cfg.env, "env", "development", "Application environment {development | production | maintenance}")
-	
-	
 
-	flag.Parse() 
+	//Run flag
+	flag.Parse()
 
 	cfg.stripe.key = os.Getenv("STRIPE_KEY")
 	cfg.stripe.secret = os.Getenv("STRIPE_SECRET")
@@ -65,15 +62,14 @@ func main(){
 	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	app := &application{
-		config: cfg,
-		infoLog:infoLog ,
+		config:   cfg,
+		infoLog:  infoLog,
 		errorLog: errorLog,
-		version: version,
-
+		version:  version,
 	}
-   err := app.serve()
-   if err != nil {
-	log.Fatal(err)
-   }
+	err := app.serve()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 }
--- cmd/api/handlers-api.go
+++ cmd/api/handlers-api.go
@@ -19,58 +19,55 @@ type jsonResponse struct {
 	ID      int    `json:"id,omitempty"`
 }
 
-func (app *application) GetPaymentIntent(w http.ResponseWriter,r *http.Request){
-	
-var payload stripePayload
+func (app *application) GetPaymentIntent(w http.ResponseWriter, r *http.Request) {
 
-err := json.NewDecoder(r.Body).Decode(&payload)
-if err != nil {
-	app.errorLog.Println("JSON decode error:", err)
-	return 
-}
+	var payload stripePayload
 
- amount ,err := strconv.Atoi(payload.Amount)
-  if err != nil {
-	app.errorLog.Println(err)
-	return
-  }
+	err := json.NewDecoder(r.Body).Decode(&payload)
+	if err != nil {
+		app.errorLog.Println("JSON decode error:", err)
+		return
+	}
 
-   card := cards.Card {
-	Secret : app.config.stripe.secret,
-	Key:app.config.stripe.key,
-	Currency : payload.Currency,
-   }
+	amount, err := strconv.Atoi(payload.Amount)
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
 
-  
-    okay := true
+	card := cards.Card{
+		Secret:   app.config.stripe.secret,
+		Key:      app.config.stripe.key,
+		Currency: payload.Currency,
+	}
 
-	pi,msg,err := card.Charge(payload.Currency,amount)
+	okay := true
+
+	pi, msg, err := card.Charge(payload.Currency, amount)
 	if err != nil {
 		okay = false
 	}
 
-	 if okay {
-            out,err := json.MarshalIndent(pi,""," ")
-			if err != nil {
-				app.errorLog.Println(err)
-				return
-			}
-			 w.Header().Set("Content-Type","application/json")
-			 w.Write(out)
-	 } else {
-     j := jsonResponse{
-		OK: false,
-		Message: msg,
-		Content:"",
-	}
+	if okay {
+		out, err := json.MarshalIndent(pi, "", " ")
+		if err != nil {
+			app.errorLog.Println(err)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(out)
+	} else {
+		j := jsonResponse{
+			OK:      false,
+			Message: msg,
+			Content: "",
+		}
 
-	out,err := json.MarshalIndent(j,"","  ")
-	if err != nil {
-		app.errorLog.Println(err)
+		out, err := json.MarshalIndent(j, "", "  ")
+		if err != nil {
+			app.errorLog.Println(err)
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(out)
 	}
-	w.Header().Set("Content-Type","application/json")
-	w.Write(out)
-	 }
-
-	
 }
