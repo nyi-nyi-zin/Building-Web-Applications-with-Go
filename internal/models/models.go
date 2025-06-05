3640d480d68dca5912eb66d65f5fb63c49be170a
nyi nyi zin <nyinyizin1818@gmail.com>
2025-06-05 09:58:27 +0630

    40. Working on database functions

--- internal/models/models.go
+++ internal/models/models.go
@@ -69,7 +69,7 @@ type Transaction struct {
 	Currency            string    `json:"currency"`
 	LastFour            string    `json:"last_four"`
 	BankReturnCode      string    `json:"bank_return_code"`
-	TransactionStatusId int       `json:"transaction_status_id"`
+	TransactionStatusID int       `json:"transaction_status_id"`
 	CreatedAt           time.Time `json:"-"`
 	UpdatedAt           time.Time `json:"-"`
 }
@@ -91,8 +91,23 @@ func (m *DBModel) GetWidget(id int) (Widget, error) {
 
 	var widget Widget
 
-	row := m.DB.QueryRowContext(ctx, "select id,name from widgets where id = ?", id)
-	err := row.Scan(&widget.ID, &widget.Name)
+	row := m.DB.QueryRowContext(ctx, `
+	select 
+	  id,name, description, inventory_level, price, coalesce(image, ''),
+	  created_at, updated_at
+	from
+	  widgets 
+    where id = ?`, id)
+	err := row.Scan(
+		&widget.ID,
+		&widget.Name,
+		&widget.Description,
+		&widget.InventoryLevel,
+		&widget.Price,
+		&widget.Image,
+		&widget.CreatedAt,
+		&widget.UpdatedAt,
+	)
 	if err != nil {
 		return widget, err
 	}
